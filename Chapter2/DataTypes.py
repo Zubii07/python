4b9e38945df4715d58c8c2047c07454f82ebe8e2
M Zohaib <167600779+Zubii07@users.noreply.github.com>
2025-02-25 14:19:55 +0500

    Add files via upload

---
+++ Chapter2/DataTypes.py
@@ -0,0 +1,5 @@
+a = 12    # a is an integer
+b = 3.33  # b is an floating point number
+c = "Hello"  # c is an string
+d = False   # d is an boolean value True/False
+e = None # e is none type variable
---
+++ Chapter2/operators.py
@@ -0,0 +1,37 @@
+# Arithmetic Operators
+
+a = 3
+b = 4
+c = a+b
+print("a + b = ", c)
+
+
+
+a = 3
+b = 4
+c = a-b
+print("a - b = ", c)
+
+
+a = 3
+b = 4
+c = a*b
+print("a * b = ", c)
+
+
+a = 12
+b = 4
+c = a/b
+print("a / b = ", c)
+
+
+
+# Assignment Operator
+t = 5 # Assign 5 to t
+b +=3  # Increment the value of U by 3 and then assign it to U
+
+
+
+
+# Comparison Operater
+  # ==,<=, >= <,>,!== .....
